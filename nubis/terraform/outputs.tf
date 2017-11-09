4486e74e364cc798851119fe70eb8ca8099b20bf
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-11-09 14:26:56 -0500

    fix outputs

--- nubis/terraform/outputs.tf
+++ nubis/terraform/outputs.tf
@@ -1,11 +1,7 @@
-output "EIP" {
-  value = "${aws_eip.hypercored.id}"
-}
-
 output "Address" {
   value = "${aws_eip.hypercored.public_ip}"
 }
 
 output "DNS" {
-  value = "https://${module.dns.fqdn}/"
+  value = "http://${module.dns.fqdn}:3283/"
 }
